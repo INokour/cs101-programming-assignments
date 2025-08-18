9476430802c716e7a174eee690e23bd00ae8b114
Cautioned <youfoundibrahim@gmail.com>
2025-08-18 14:30:29 -0400

    week1 test

---
+++ week01/homework/week1_test.py
@@ -0,0 +1,6 @@
+print("Hello from VS Code!")
+print("GitHub integration is working!")
+# Test basic Python functionality
+name = "Ibrahim Nokour"
+course = "CS 101"
+print(f"I am {name} and I'm taking {course}")
